04cda24975f65e962f1356c0260199bc7881ed6f
Drareg04 <gerard.lorza@gmail.com>
2026-06-21 21:54:00 +0200

    Sprint 0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,11 +13,11 @@
 <div class="featureddiv">
     <h1>Featured</h1>
     <div class="featuredimgdiv">
-        <a class="featured"><img src="/img/carousel/Valentains_discount.webp" alt="Valentains discount Up to 95% promotion"></a>
+        {{-- <a class="featured"><img src="/img/carousel/Valentains_discount.webp" alt="Valentains discount Up to 95% promotion"></a>
         <a class="featured"><img src="/img/carousel/Wawelin.webp" alt="Halloween discount Up to 85% promotion"></a>
     </div>
     <a><img class="featuredarrow" src="/img/carousel/leftarrow.svg" alt="leftarrow" onclick="carouselleft()"></a>
-    <a><img class="featuredarrow" src="/img/carousel/rightarrow.svg" alt="rightarrow" style="left: 94.5vw" onclick="carousel()"></a>
+    <a><img class="featuredarrow" src="/img/carousel/rightarrow.svg" alt="rightarrow" style="left: 94.5vw" onclick="carousel()"></a> --}}
 </div>
 {{-- recommended --}}
 
